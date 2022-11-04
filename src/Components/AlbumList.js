1c197c58ec8e010949ef61d94298c2720c2f3068
Daniel Guzman <guzdan@live.com>
2022-11-04 12:37:03 -0500

    Adds a fetch to AlbumList and sets state to returned data

--- src/Components/AlbumList.js
+++ src/Components/AlbumList.js
@@ -1,9 +1,23 @@
 import React from "react";
+import { useState, useEffect } from "react";
 
 function AlbumList(){
+    const [albums, setAlbums] = useState([])
+    
+    
+    useEffect(() => {
+        fetch("http://localhost:3001/albums")
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+            setAlbums(data)
+        })
+    }, [])
+
+
     return (
         <div>
-            This the list of your favorite albums
+            <h2>This the list of your favorite albums</h2>
         </div>
     )
 }
