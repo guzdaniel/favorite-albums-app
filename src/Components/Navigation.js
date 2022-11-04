76d9739f607d8dd559d15b78eda83d2d0c8525e4
Daniel Guzman <guzdan@live.com>
2022-11-04 12:02:15 -0500

    Adds styling to navigation bar

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -2,14 +2,51 @@ import React from "react";
 
 import { NavLink } from "react-router-dom";
 
+const linkStyles = {
+    display: "inline-block",
+    padding: "1rem 4rem 1rem",
+    margin: "0.2rem",
+    background: "Black",
+    textDecoration: "none",
+    color: "lightgreen",
+    fontsize: "1.2em"
+
+};
 
 
 function Navigation() {
     return (
         <nav>
-            <NavLink to="/"> Home </NavLink>
-            <NavLink to="/albums"> Albums </NavLink>
-            <NavLink to="/albums/new"> Add An Album </NavLink>
+            <NavLink
+                to="/"
+                exact
+                style={linkStyles}
+                activeStyle={{
+                    color: "white",
+                }}
+            >
+                Home
+            </NavLink>
+            <NavLink
+                to="/albums"
+                exact
+                style={linkStyles}
+                activeStyle={{
+                    color: "white",
+                }}
+            >
+                My Albums
+            </NavLink>
+            <NavLink
+                to="/albums/new"
+                exact
+                style={linkStyles}
+                activeStyle={{
+                    color: "white",
+                }}
+            >
+                Add An Album
+            </NavLink>
         </nav>
     );
 }
