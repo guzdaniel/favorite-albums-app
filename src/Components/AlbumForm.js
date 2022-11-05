c17dbd116835981a2cd43cbe774f7454956c2a11
Daniel Guzman <guzdan@live.com>
2022-11-04 22:12:50 -0500

    Adds intial form functionality for title input

--- src/Components/AlbumForm.js
+++ src/Components/AlbumForm.js
@@ -1,9 +1,44 @@
 import React from "react";
+import { useState } from "react";
 
-function AlbumForm(){
+function AlbumForm() {
+
+    const [title, setTitle] = useState("")
+
+    function handleSubmit(e) {
+        e.preventDefault()
+        console.log("submitted")
+    }
+
+    function handleChange(e) {
+        setTitle(e.target.value)
+    }
+
+    console.log("the title " + title)
+
     return (
         <div>
-            This is the form to add and album to your favorites
+            <section>
+                <form className="container" onSubmit={handleSubmit}>
+                    <label htmlFor="title">Album Title: </label>
+                    <input type="text" id="title" name="title" placeholder="Enter Title"
+                        value={title} onChange={handleChange} /> <br/>
+{/* 
+                    <label htmlFor="title">Artist: </label>
+                    <input type="text" id="artist" name="artist" placeholder="Enter Artist"
+                        value="" onChange={handleChange} /> <br/>
+
+                    <label htmlFor="title">Image URL: </label>
+                    <input type="text" id="image" name="image" placeholder="Enter URL"
+                        value="" onChange={handleChange} /> <br/>
+
+                    <label htmlFor="title">Rating: </label>
+                    <input type="text" id="rating" name="rating" placeholder="Enter Rating 1-4"
+                        value="" onChange={handleChange} /> <br/> */}
+
+                    <input type="submit" value="Submit"/>
+                </form>
+            </section>
         </div>
     )
 }
