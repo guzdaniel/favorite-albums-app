f44ff80f5088dbee26ef2b3428acdc988ee4fbf5
Daniel Guzman <guzdan@live.com>
2022-11-07 10:55:06 -0600

    Adds artist, image and rating input fields to Form page

--- src/Components/AlbumForm.js
+++ src/Components/AlbumForm.js
@@ -13,13 +13,15 @@ function AlbumForm( {addAlbum} ) {
     function handleSubmit(e) {
         e.preventDefault() 
         console.log("submitted")
+        console.log(album)
+       
     }
 
     function handleChange(e) {
         setAlbum({...album, [e.target.name]: e.target.value})
     }
 
-    console.log("the album " + album.title)
+    console.log("the album " + album.title + album.artist + album.image + album.rating)
 
     return (
         <div>
@@ -28,18 +30,18 @@ function AlbumForm( {addAlbum} ) {
                     <label htmlFor="title">Album Title: </label>
                     <input type="text" id="title" name="title" placeholder="Enter Title"
                         value={album.title} onChange={handleChange} /> <br/>
-{/* 
+
                     <label htmlFor="title">Artist: </label>
                     <input type="text" id="artist" name="artist" placeholder="Enter Artist"
-                        value="" onChange={handleChange} /> <br/>
+                        value={album.artist} onChange={handleChange} /> <br/>
 
                     <label htmlFor="title">Image URL: </label>
                     <input type="text" id="image" name="image" placeholder="Enter URL"
-                        value="" onChange={handleChange} /> <br/>
+                        value={album.image} onChange={handleChange} /> <br/>
 
                     <label htmlFor="title">Rating: </label>
                     <input type="text" id="rating" name="rating" placeholder="Enter Rating 1-4"
-                        value="" onChange={handleChange} /> <br/> */}
+                        value={album.rating} onChange={handleChange} /> <br/>
 
                     <input type="submit" value="Submit"/>
                 </form>
