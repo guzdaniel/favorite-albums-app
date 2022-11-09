39d0aca0c269b91f0dffe404bf58a1daec94bbdf
Daniel Guzman <guzdan@live.com>
2022-11-09 15:40:21 -0600

    Updates number of stars allowed for each album

--- src/Components/AlbumForm.js
+++ src/Components/AlbumForm.js
@@ -55,7 +55,7 @@ function AlbumForm({ addAlbum }) {
                     value={albumForm.image} onChange={handleChange} /> <br />
 
                 <label htmlFor="title">Rating: </label>
-                <input type="number" id="rating" name="rating" min="1" max="4" required
+                <input type="number" id="rating" name="rating" min="1" max="5" required
                     value={albumForm.rating} onChange={handleChange} /><br />
 
                 <input type="submit" value="Submit" />
