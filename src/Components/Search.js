08476cce7329e0acd5f076f10892e454f4c15231
Daniel Guzman <guzdan@live.com>
2022-12-02 14:23:42 -0600

    Adds state to Search component to handle user input

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,19 +1,24 @@
 import React from "react";
+import { useState } from "react";
 
 function Search(){
 
+    const [searchForm, setSearchForm] = useState("")
+
 
     function handleChange(e) {
-        console.log(e.target.value)
+        setSearchForm(e.target.value)
+        console.log(searchForm)
     }
 
+   
     
 
     return(
         <div>
             Search for an album title:
             <form>
-                <input type="text" name="search" defaultValue="" onChange={handleChange} />
+                <input type="text" name="search" value={searchForm} onChange={handleChange} />
             </form>
         </div>
     )
